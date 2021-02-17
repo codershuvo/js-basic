4863e5a50ecf12d84b0d48c922428d84009943dc
codershuvo <nisho.mushfiq2020@gmail.com>
2021-02-17 07:50:17 +0600

    variable added

---
+++ variable.js
@@ -0,0 +1,31 @@
+// variable 
+var bananaPrice = 12; 
+console.log(bananaPrice); 
+
+
+var temperature = 19; 
+console.log(temperature); 
+
+var age = 23; 
+console.log(age); 
+
+
+var phonePrice = 13000; 
+console.log(phonePrice);
+
+
+// variable type 
+var seenAfter = 21; 
+console.log(typeof seenAfter); 
+
+var myName = 'Mosfiqur Rahman'; 
+console.log(typeof myName); 
+
+var isHot = true; 
+var isRich = false; 
+console.log(typeof isHot); 
+
+var friendsName; 
+console.log(typeof friendsName); 
+
+
